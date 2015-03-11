ebe3eddce718ed59c7a528957d5573315ec209a2
aWang <aWang@sanrenxing.tv>
2015-03-11 20:33:01 +0800

    目前基本完成,待完善了(Preview04)

--- src/tv/sanrenxing/awang/swatches/Swatches.java
+++ src/tv/sanrenxing/awang/swatches/Swatches.java
@@ -37,6 +37,10 @@ public class Swatches extends View {
 	/*** 整个View的高度 */
 	private int mHeight = 0;
 
+	/*** 每一度需要跨越的RGB色值 */
+	// TODO：考虑一下，这里应该是255还是256呢 //
+	private static double rgbValuePerDegree = 256 / 60;
+
 	public Swatches(Context context) {
 		this(context, null);
 	}
@@ -101,50 +105,182 @@ public class Swatches extends View {
 	protected void onDraw(Canvas canvas) {
 		Log.d(VIEW_LOG_TAG, "onDraw()");
 		super.onDraw(canvas);
-		canvas.drawCircle(cx, cy, radius, paint);
-		// 二级颜色 //
+		drawColorCircle(canvas);
+	}
+
+	/**
+	 * 绘制色盘
+	 * 
+	 * @param canvas
+	 */
+	protected void drawColorCircle(Canvas canvas) {
 		int red = 0;
 		int green = 0;
 		int blue = 0;
-		for (int i = 0; i < 60; i++) {
-			green = 255 / 60 * i;
-			paint.setColor(Color.rgb(255, green, 0));
-			canvas.drawArc(oval, i, 1, true, paint);
-		}
-		
-		// paint.setColor(Color.BLACK); //
-		// canvas.drawLine(cx, cy, cx + radius * (float) Math.cos(Math.PI / 3), cy	+ radius * (float) Math.sin(Math.PI / 3), paint); //
-		
-		for (int i = 60; i < 120; i++) {
-			red = 255 - 256 / 60 * (i - 60);
-			paint.setColor(Color.rgb(red, 255, 0));
-			canvas.drawArc(oval, i, 1, true, paint);
-		}
-		
-		for (int i = 120; i < 180; i++) {
-			blue = 256 / 60 * (i - 120);
-			paint.setColor(Color.rgb(0, 255, blue));
-			canvas.drawArc(oval, i, 1, true, paint);
-		}
-		for (int i = 180; i < 240; i++) {
-			green = 255 - 256 / 60 * (i - 180);
-			paint.setColor(Color.rgb(0, green, 255));
-			canvas.drawArc(oval, i, 1, true, paint);
-		}
-		for (int i = 240; i < 300; i++) {
-			red = 256 / 60 * (i - 240);
-			paint.setColor(Color.rgb(red, 0, 255));
-			canvas.drawArc(oval, i, 1, true, paint);
-		}
-		for (int i = 300; i < 360; i++) {
-			blue = 255 - 256 / 60 * (i - 300);
-			paint.setColor(Color.rgb(255, 0, blue));
-			canvas.drawArc(oval, i, 1, true, paint);
+		for (int i = 0; i < 360; i++) {
+			if (i < 60) {
+				green = (int) (rgbValuePerDegree * i);
+				paint.setColor(Color.rgb(255, green, 0));
+				canvas.drawArc(oval, i, 1, true, paint);
+			} else if (i < 120) {
+				red = (int) (255 - rgbValuePerDegree * (i - 60));
+				paint.setColor(Color.rgb(red, 255, 0));
+				canvas.drawArc(oval, i, 1, true, paint);
+			} else if (i < 180) {
+				blue = (int) (rgbValuePerDegree * (i - 120));
+				paint.setColor(Color.rgb(0, 255, blue));
+				canvas.drawArc(oval, i, 1, true, paint);
+			} else if (i < 240) {
+				green = (int) (255 - rgbValuePerDegree * (i - 180));
+				paint.setColor(Color.rgb(0, green, 255));
+				canvas.drawArc(oval, i, 1, true, paint);
+			} else if (i < 300) {
+				red = (int) (rgbValuePerDegree * (i - 240));
+				paint.setColor(Color.rgb(red, 0, 255));
+				canvas.drawArc(oval, i, 1, true, paint);
+			} else if (i < 360) {
+				blue = (int) (255 - rgbValuePerDegree * (i - 300));
+				paint.setColor(Color.rgb(255, 0, blue));
+				canvas.drawArc(oval, i, 1, true, paint);
+			}
 		}
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		Log.d(VIEW_LOG_TAG, "getX : " + event.getX());
+		Log.d(VIEW_LOG_TAG, "getY : " + event.getY());
+
+		float x = event.getX();
+		float y = event.getY();
+		// 象限 //
+		int quadrant = 0;
+		if (x > cx) {
+			if (y > cy) {
+				quadrant = 1;
+			} else if (y < cy) {
+				quadrant = 4;
+			}
+		} else if (x < cx) {
+			if (y > cy) {
+				quadrant = 2;
+			} else if (y < cy) {
+				quadrant = 3;
+			}
+		}
+		Log.d(VIEW_LOG_TAG, "Quadrant : " + quadrant);
+		// 距离 //
+		float distance = (float) Math.sqrt((Math.pow(x - cx, 2) + Math.pow(y
+				- cy, 2)));
+		Log.d(VIEW_LOG_TAG, "Distance : " + distance);
+		if (distance <= radius) {
+			Log.d(VIEW_LOG_TAG, "Inner");
+			int anger = 0;
+			double radian = 0;
+			switch (quadrant) {
+			case 1:
+				radian = Math.asin((y - cy) / radius);
+				break;
+			case 2:
+				radian = Math.asin((cx - x) / radius) + Math.PI / 2;
+				break;
+			case 3:
+				radian = Math.asin((cy - y) / radius) + Math.PI;
+				break;
+			case 4:
+				radian = Math.asin((x - cx) / radius) + Math.PI / 2 * 3;
+				break;
+			}
+			anger = (int) (radian / Math.PI * 180);
+			Log.d(VIEW_LOG_TAG, "Anger : " + anger);
+			Log.d(VIEW_LOG_TAG, "Radian : " + radian);
+			int color = getColor(anger);
+			if (colorSelectedListener != null) {
+				colorSelectedListener.onColorSelected(color);
+			}
+		} else {
+			Log.d(VIEW_LOG_TAG, "Outer");
+		}
 		return super.onTouchEvent(event);
+	}
+
+	protected enum RGB {
+		R, G, B
+	};
+
+	protected class RGBColor {
+		public int R;
+		public int G;
+		public int B;
+
+		@Override
+		public String toString() {
+			return "RGBColor [R=" + R + ", G=" + G + ", B=" + B + "]";
+		}
+	}
+
+	// 六个弧度区域内RGB的最大/小值 //
+	protected static int[] rgbAreaR = new int[] { 255, 255, 0, 0, 0, 255, 255 };
+	protected static int[] rgbAreaG = new int[] { 0, 255, 255, 255, 0, 0, 0 };
+	protected static int[] rgbAreaB = new int[] { 0, 0, 0, 255, 255, 255, 0 };
+
+	// 上面数组的整合版本 //
+	protected static int[][] rgbArea = { //
+	{ 255, 255, 0, 0, 0, 255, 255 }, //
+			{ 0, 255, 255, 255, 0, 0, 0 }, //
+			{ 0, 0, 0, 255, 255, 255, 0 } //
+	};
+
+	// 当前弧度内变化的是R/G/B(平均分成6个弧度,每个弧度PI/2,PI=180°) //
+	protected static RGB[] rgbAreaWho = new RGB[] { RGB.G, RGB.R, RGB.B, RGB.G,
+			RGB.R, RGB.B };
+	// 度数增加还是减少 //
+	protected static int[] rgbAreaPM = new int[] { 1, -1, 1, -1, 1, -1 };
+
+	protected RGBColor getRGBColor(int anger) {
+		RGBColor rgb = new RGBColor();
+		int area = anger / 60;
+		int remainder = anger % 60;
+		Log.d(VIEW_LOG_TAG, "Area : " + area);
+		int pm = rgbAreaPM[area];
+		RGB who = rgbAreaWho[area];
+		rgb.R = rgbAreaR[area];
+		rgb.G = rgbAreaG[area];
+		rgb.B = rgbAreaB[area];
+		double diffVal = (remainder / 60.0) * 255;
+		Log.d(VIEW_LOG_TAG, "DiffVal : " + diffVal);
+		switch (who) {
+		case R:
+			rgb.R += pm * diffVal;
+			break;
+		case G:
+			rgb.G += pm * diffVal;
+			break;
+		case B:
+			rgb.B += pm * diffVal;
+			break;
+		}
+		Log.d(VIEW_LOG_TAG, rgb.toString());
+		return rgb;
+	}
+
+	protected int getColor(int anger) {
+		RGBColor rgb = getRGBColor(anger);
+		return Color.rgb(rgb.R, rgb.G, rgb.B);
+	}
+
+	private ColorSelectedListener colorSelectedListener = null;
+
+	public ColorSelectedListener getColorSelectedListener() {
+		return colorSelectedListener;
+	}
+
+	public void setColorSelectedListener(
+			ColorSelectedListener colorSelectedListener) {
+		this.colorSelectedListener = colorSelectedListener;
+	}
+
+	public interface ColorSelectedListener {
+		public void onColorSelected(int color);
 	}
 }
--- src/tv/sanrenxing/awang/swatches/demo/MainActivity.java
+++ src/tv/sanrenxing/awang/swatches/demo/MainActivity.java
@@ -1,14 +1,32 @@
 package tv.sanrenxing.awang.swatches.demo;
 
 import tv.sanrenxing.awang.swatches.R;
+import tv.sanrenxing.awang.swatches.Swatches;
+import tv.sanrenxing.awang.swatches.Swatches.ColorSelectedListener;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView textView = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		textView = (TextView) findViewById(R.id.textView);
+		Swatches swatches = (Swatches) findViewById(R.id.swatches);
+		swatches.setColorSelectedListener(new ColorSelectedListener() {
+
+			@Override
+			public void onColorSelected(int color) {
+				textView.setBackgroundColor(color);
+				textView.setTextColor(Color.WHITE);
+				textView.setText("Color : " + color);
+			}
+		});
 	}
 }
